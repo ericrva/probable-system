04abf9e66992eab4c51a28e4efb841a5eccd5c32
ericrva <eric.rva@gmail.com>
2017-04-27 15:07:25 -0300

    upload inicial

---
+++ CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,38 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto pmodel)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(pmodel);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ CaelumEstoque/CaelumEstoque/Migrations/201704270101561_inicial.cs
@@ -0,0 +1,56 @@
+namespace CaelumEstoque.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class inicial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CategoriaDoProduto",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Descricao = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Produto",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Preco = c.Single(nullable: false),
+                        CategoriaId = c.Int(),
+                        Descricao = c.String(),
+                        Quantidade = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.CategoriaDoProduto", t => t.CategoriaId)
+                .Index(t => t.CategoriaId);
+            
+            CreateTable(
+                "dbo.Usuario",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Senha = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Produto", "CategoriaId", "dbo.CategoriaDoProduto");
+            DropIndex("dbo.Produto", new[] { "CategoriaId" });
+            DropTable("dbo.Usuario");
+            DropTable("dbo.Produto");
+            DropTable("dbo.CategoriaDoProduto");
+        }
+    }
+}
